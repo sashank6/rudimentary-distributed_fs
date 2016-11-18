fc6f183ef42359b17256fd781629652697e72c05
sashank6 <sashank.alladi@live.com>
2016-11-18 14:37:04 -0600

    Modified client code

--- Network/client.cpp
+++ Network/client.cpp
@@ -1,43 +1,16 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<stdlib.h>
-#include<string.h>
-#include<sys/socket.h>
-#include<sys/un.h>
-#include<errno.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include"ports.h"
+#include "ports.h"
+#include "connections.h"
+#include "errors.h"
+#include "Packet.pb.h"
 
-
-void handle_error(char *s) {
-	printf("Error: %s Reason: %s\n", s, strerror(errno));
-	exit(EXIT_FAILURE);
-}
+using namespace std;
 
 int main() {
 
-	int socket_fd;
-	struct sockaddr_in serv_addr;
+	Packet packet;
 
-	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (socket_fd == -1)
-		handle_error("Socket Opening Error");
-
-	struct hostent *server;
-	server = gethostbyname("192.168.1.115");
-	if (server == NULL)
-		handle_error("No valid host");
-
-	serv_addr.sin_family = AF_INET;
-	bcopy((char *) server->h_addr, (char *) &serv_addr.sin_addr.s_addr,
-			server->h_length);
-	serv_addr.sin_port = htons(SERVER_CONNECT_PORT);
-
-	if (connect(socket_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr))
-			== -1)
-		handle_error("Connection Error");
-	close(socket_fd);
+	send_message("192.168.1.115", SERVER_CONNECT_PORT, packet);
 
 	return 0;
+
 }
--- Network/connections.h
+++ Network/connections.h
@@ -1,2 +1,3 @@
+#include "Packet.pb.h"
 void open_socket(unsigned int);
 void send_message(char *hostname, unsigned int port, Packet packet);
