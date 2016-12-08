89c4480210f29f7453524d75d24d5b2f21ba43b3
sashank6 <sashank.alladi@live.com>
2016-12-07 18:47:22 -0600

    completed file list update

--- Network/client.cpp
+++ Network/client.cpp
@@ -8,16 +8,16 @@
 
 int main() {
 
-//	Packet handshake_packet;
-//	handshake_packet.set_flag(1);
-//	SystemInformation *sysinfo(new SystemInformation);
-//	sysinfo->set_port(CLIENT_PORT);
-//	sysinfo->set_disksize(1000);
-//	handshake_packet.set_allocated_sysinfo(sysinfo);
-//	int result=send_message(SERVER_ADDR, SERVER_CONNECT_PORT, handshake_packet);
-	 Packet pck=getFileList();
-	 int result=send_message(SERVER_ADDR,SERVER_CONNECT_PORT,pck);
-	//open_socket(CLIENT_PORT);
+	Packet handshake_packet;
+	handshake_packet.set_flag(1);
+	SystemInformation *sysinfo(new SystemInformation);
+	sysinfo->set_port(CLIENT_PORT);
+	sysinfo->set_disksize(1000);
+	handshake_packet.set_allocated_sysinfo(sysinfo);
+	int result=send_message(SERVER_ADDR, SERVER_CONNECT_PORT, handshake_packet);
+	Packet pck=getFileList();
+	result=send_message(SERVER_ADDR,SERVER_CONNECT_PORT,pck);
+	open_socket(CLIENT_PORT);
 
 
 	return 0;
