1f5550775b0f05aaf0316c35e45229d9751fd34b
sashank6 <sashank.alladi@live.com>
2016-12-07 19:42:36 -0600

    removed boost

--- Apps/write.cpp
+++ Apps/write.cpp
@@ -1,27 +1,27 @@
 /*
  *
  */
-#include <boost/interprocess/shared_memory_object.hpp>
-#include <boost/interprocess/mapped_region.hpp>
+//#include <boost/interprocess/shared_memory_object.hpp>
+//#include <boost/interprocess/mapped_region.hpp>
 #include<iostream>
-#include "nodes.h"
+//#include "nodes.h"
 #include "localfileops.h"
 #include "../Network/filealloc.h"
 int main(int argc, char *argv[]) {
-	if (argc < 3) {
-		std::cout << "Invalid arguments" << std::endl;
-	}
-	std::string read_file = std::string(argv[1]);
-	std::string result_file = std::string(argv[2]);
-	std::string data = readFile(read_file);
-	std::cout<<data<<std::endl;
-	Sysinfo best_node=getBestNode();
-	filerecord record;
-	record.filename=result_file;
-	record.host=best_node.ipaddress;
-	record.size=0;
-	update_fat(record);
-	write_to_node(best_node,result_file,data);
+//	if (argc < 3) {
+//		std::cout << "Invalid arguments" << std::endl;
+//	}
+//	std::string read_file = std::string(argv[1]);
+//	std::string result_file = std::string(argv[2]);
+//	std::string data = readFile(read_file);
+//	std::cout<<data<<std::endl;
+//	Sysinfo best_node=getBestNode();
+//	filerecord record;
+//	record.filename=result_file;
+//	record.host=best_node.ipaddress;
+//	record.size=0;
+//	update_fat(record);
+//	write_to_node(best_node,result_file,data);
 	return 0;
 }
 
